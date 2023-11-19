70cc40f3bfeb01fda94b8f2f3f7502393a976441
Saheed Adeniyi <52088364+swallahfx@users.noreply.github.com>
2023-11-19 12:22:44 +0100

    Update models.py
    profile-management

--- todoproject/users/models.py
+++ todoproject/users/models.py
@@ -3,3 +3,8 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, max_length=2000)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    date_of_birth = models.DateField(null=True)
+    location = models.CharField(max_length=255, blank=True)
+    website = models.URLField(blank=True)
