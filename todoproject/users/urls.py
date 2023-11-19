e943b403dd0bd4e370d6474813976e65f049fc68
Saheed Adeniyi <52088364+swallahfx@users.noreply.github.com>
2023-11-19 12:26:00 +0100

    Update urls.py
    refresh token

--- todoproject/users/urls.py
+++ todoproject/users/urls.py
@@ -4,4 +4,7 @@ from .views import RegisterView, LoginView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('profiles/', UserProfileListCreateView.as_view(), name='user_profile_list_create'),
+    path('profiles/<int:pk>/', UserProfileRetrieveUpdateDestroyView.as_view(), name='user_profile_retrieve_update_destroy'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
